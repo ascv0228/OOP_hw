7c20f586ba0c11b5841d5ce9b052ac456f10ac8f
qihuan <77038091+ascv0228@users.noreply.github.com>
2023-11-25 23:56:04 +0800

    update

--- src/main/java/com/http/MongodbConnect.java
+++ src/main/java/com/http/MongodbConnect.java
@@ -7,8 +7,8 @@ import com.mongodb.MongoClientSettings;
 import com.mongodb.ServerApi;
 import com.mongodb.ServerApiVersion;
 
+import java.util.Base64;
 import java.util.logging.Handler;
-
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,17 +18,17 @@ public class MongodbConnect {
     public static MongoClient connectToMongoDB() {
         // MongoDB connection code
 
-        String uriString = "mongodb+srv://b083022053:TEST@ascv0228.t0qjgal.mongodb.net/?retryWrites=true&w=majority";
+        String uriString = "bW9uZ29kYitzcnY6Ly9URVNUOlRFU1RAYXNjdjAyMjgudDBxamdhbC5tb25nb2RiLm5ldC8/cmV0cnlXcml0ZXM9dHJ1ZSZ3PW1ham9yaXR5";
         Logger mongoLogger = Logger.getLogger("org.mongodb.driver");
         for (Handler handler : mongoLogger.getHandlers()) {
             handler.setLevel(Level.WARNING);
         }
-        System.out.println("run: " + uriString);
+        System.out.println("run: connectToMongoDB");
         ServerApi serverApi = ServerApi.builder()
                 .version(ServerApiVersion.V1)
                 .build();
         MongoClientSettings settings = MongoClientSettings.builder()
-                .applyConnectionString(new ConnectionString(uriString))
+                .applyConnectionString(new ConnectionString(new String(Base64.getDecoder().decode(uriString))))
                 .serverApi(serverApi)
                 .build();
 
